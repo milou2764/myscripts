0fb99b6a6b8f350ef139c9fc929ec4c833ce5e94
milou2764 <emilien.andre2764@gmail.com>
2024-02-10 15:15:29 +0100

    fix

--- previewtex.sh
+++ previewtex.sh
@@ -4,13 +4,17 @@ myfile="$1"
 fwoext="${myfile%.*}"
 mydvi="$fwoext.dvi"
 
-compile_and_show() {
-	tex &latex "$myfile"
-	xdvi -unique $mydvi &
+compile() {
+	latex "$myfile"
 }
 
-compile_and_show
+compile_and_show() {
+	compile
+	xdvi -unique $mydvi
+}
 
+compile
+xdvi -unique $mydvi &
 inotifywait -m -e close_write "$1" | 
    while read file_path file_event file_name; do 
 	   echo ${file_path}${file_name} event: ${file_event}
